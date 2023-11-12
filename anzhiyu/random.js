ec94cd59ea745437a3a2ff496cc5d3111bfdbb16
OhtoAi1024 <muzhi567@qq.com>
2023-11-12 19:40:13 +0800

    Site updated: 2023-11-12 19:40:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3.html","posts/6.html","posts/2.html","posts/3.html","posts/3.html","posts/4.html","posts/4.html","posts/3.html","posts/3.html","posts/3.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/3.html","posts/6.html","posts/3.html","posts/3.html","posts/2.html","posts/4.html","posts/3.html","posts/4.html","posts/3.html","posts/3.html","posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
